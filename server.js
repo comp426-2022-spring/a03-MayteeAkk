6d6bc9f376ad78e8c4f6353f1ad7d96c75b416a8
MayteeAkk <mayteeoakk@gmail.com>
2022-03-01 00:06:38 -0500

    Changed Syntax

--- server.js
+++ server.js
@@ -6,20 +6,44 @@ import express from "express";
 const app = express();
 
 const args = (minimist)(process.argv.slice(2));
-var HTTP_PORT = process.env.PORT || 5000;
+let port = process.env.PORT || 5000;
 
-//Starting an App Server
-const server = app.listen(HTTP_PORT, () => {
-    console.log("App listening on port %PORT%".replace("%PORT%", HTTP_PORT));
-});
+// //Starting an App Server
+// app.listen(port, () => {
+//     console.log("App listening on port %PORT%".replace("%PORT%", port));
+// });
 
-//Default Reponse for Any Other Request
-app.use(function (req, res) {
-    res.status(404).send("404 NOT FOUND");
-    res.type("text/plain");
-});
+// //Default Reponse for Any Other Request
+// app.use(function (req, res) {
+//     res.status(404).send("404 NOT FOUND");
+//     res.type("text/plain");
+// });
 
-//Defining Check Endpoint
+// //Defining Check Endpoint
+// app.get('/app/flip', (req, res) => {
+//     res.status(200).json({"flip": coinFlip()})
+// })
+
+// app.get('/app/flips/:number', (req, res) => {
+//     const result = coinFlips(parseInt(req.params.number))
+//     const count = countFlips(result)
+//     res.status(200).json({"raw": result, "summary": count})
+// })
+
+// app.get('/app/flip/call/:call', (req, res) => {
+//     res.status(200).json(flipACoin(req.params.call))
+// })
+
+// app.get('/app', (req, res) => {
+//     res.statusMessage = 'OK';
+//     res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+//     res.end(res.statusCode+ ' ' +res.statusMessage)
+// })
+
+const server = app.listen(port, () => {
+    console.log('App is running on port %PORT%'.replace('%PORT%', port))
+})
+
 app.get('/app/flip', (req, res) => {
     res.status(200).json({"flip": coinFlip()})
 })
@@ -38,6 +62,11 @@ app.get('/app', (req, res) => {
     res.statusMessage = 'OK';
     res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
     res.end(res.statusCode+ ' ' +res.statusMessage)
+})
+
+app.use(function(req, res) {
+    res.status(404).send("404 NOT FOUND")
+    res.type("text/plain")
 })
 
 
